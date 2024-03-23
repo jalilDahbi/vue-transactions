1662669b2c28156f64701a41416c8e4e3f021b39
jalilDahbi <jalilmarocmd@gmail.com>
2024-03-23 15:37:22 +0000

    custmised transition

--- src/App.vue
+++ src/App.vue
@@ -33,11 +33,12 @@ min-height: 200px;
 }
 .v-enter-active,
 .v-leave-active {
-  transition: opacity 0.5s ease;
+  transition: opacity 0.5s ease, transform  0.5s ease;
 }
 
 .v-enter-from,
 .v-leave-to {
   opacity: 0;
+  transform: translateY(-10px);
 }
 </style>
