57aeb89bc711b458de9412fad74aadfbd5a03cd1
jalilDahbi <jalilmarocmd@gmail.com>
2024-03-25 01:03:55 +0000

    nested vue duration and explicit transition durations

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref } from 'vue';
 
-const showContent = ref(false)
+const showModel = ref(false)
 </script>
 
 <template>
@@ -10,39 +10,72 @@ const showContent = ref(false)
   </header>
 
   <main class="main">
-    <button @click="showContent = !showContent">
-      {{ showContent ? "Hide" : "Show" }} Content
+    <button @click="showModel = true">
+        show model
     </button>
-    <transition>
-      <ul v-if="showContent">
-        <li>item1</li>
-        <li>item3</li>
-        <li>item3</li>
-      </ul>
+    <transition :duration="1400">
+    
+      <div v-if="showModel" class="model-wrapper">
+        <div @click="showModel = false" class="model-bg">
+
+        </div>
+        <div class="model">
+          <button class="model-close" @click="showModel = false">X</button>
+          <div class="model-content">hello world</div>
+        </div>
+      </div>
     </transition>
   </main>
 </template>
 
 <style scoped>
-.main {
-background-color: aqua;
-padding: 20px;
-border: 3px solid rgb(2, 62, 62);
-border-radius: 10px;
-min-height: 200px;
+.model-wrapper,
+.model-bg {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
-.v-enter-active,
-.v-leave-active {
-  transition: opacity 0.5s ease, transform  0.5s ease;
+.model-bg{
+  background: rgba(0, 0, 0, 0.4);
+}
+.model,
+.model-bg{
+  transition: all 1s ease;
+}
+.model {
+  position: relative;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-shadow: 0 1px 5px rgba(0, 0, 0, 0.2);
+  padding: 20px;
+  z-index: 2;
+  
+}
+.model-close{
+  position: absolute;
+  top : 0;
+  right: 1px;
+  padding: 5px;
+  appearance: none;
+  background: none;
+  border: none;
+  font-size: 15px;
+
+}
+.v-enter-active .model{
+  transition-delay: 0.4s;
+
+}
+.v-enter-from .model,
+.v-leave-to .model{
+  opacity: 0;
+  transform: translateY(-30px);
 }
 
-.v-enter-from,
-.v-leave-to {
-  opacity: 0;
-  transform: translateY(-10px);
-}
-.v-leave-to {
-  opacity: 0;
-  transform: translateY(10px);
-}
 </style>
